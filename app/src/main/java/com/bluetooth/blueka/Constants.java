d220bcd1ad759f08c18573ec61b7d88d3db24321
qkloh <lohqiankai@gmail.com>
2020-10-21 22:45:04 +0800

    FirstBasicScan

---
+++ app/src/main/java/com/bluetooth/blueka/Constants.java
@@ -0,0 +1,8 @@
+package com.bluetooth.blueka;
+
+public class Constants {
+    public static final String TAG = "BlueKa";
+    public static final String FIND = "Find BlueKa Devices";
+    public static final String STOP_SCANNING = "Stop Scanning";
+    public static final String SCANNING = "Scanning";
+}
---
+++ app/src/main/java/com/bluetooth/blueka/ui/PeripheralControlActivity.java
@@ -0,0 +1,7 @@
+package com.bluetooth.blueka.ui;
+import android.app.Activity;
+
+public class PeripheralControlActivity extends Activity {
+    public static final String EXTRA_NAME="name";
+    public static final String EXTRA_ID = "id";
+}
